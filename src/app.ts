3d5e2018729474cb96f17495b5cb2ff3290e429f
prantobiswas321 <rahulbd321@gmail.com>
2023-06-08 23:05:59 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,11 @@ const app: Application = express()
 app.use(cors())
 
 // parser
-          app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // testing
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Working Successfully!')
 })
 
